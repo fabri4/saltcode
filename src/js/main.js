9b1b2849ee4facbefd3b02c9750eafa8401e37b6
fabri4 <khomenkoos@yandex.ru>
2015-10-28 22:09:30 +0100

    - added first and second sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,20 @@
-
 $(document).ready(function() {
+    //////////     MENU       ////////////
+
+    var $header = $(".b-header"),
+        $menulink = $(".menu-link"),
+        $headerMenu = $(".b-header__nav");
+    $menulink.on("click", function() {
+        $header.add($headerMenu).toggleClass("active");
+    });
+
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 50) {
+            $header.addClass('b-header--scroll');
+        }
+        else {
+            $header.removeClass('b-header--scroll');
+        }
+    });
 
 });
